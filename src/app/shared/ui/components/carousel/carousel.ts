3581edec2ec98ba20f93c90db0cfe5637f1c6172
Bitee-cd <calebduniya45@gmail.com>
2023-11-24 11:20:03 +0100

    updates for team members

--- src/app/shared/ui/components/carousel/carousel.ts
+++ src/app/shared/ui/components/carousel/carousel.ts
@@ -7,10 +7,10 @@ export const carousel = [
   { title: 'BayRock', url: '/assets/images/carousel/Bayrock.png', id: 1 },
   {
     title: 'Rhalposdy',
-    url: '/assets/images/carousel/rhalposdy.png',
+    url: '/assets/images/carousel/slider2.jpg',
     id: 2,
   },
-  { title: 'Slow', url: '/assets/images/carousel/slow.jpeg', id: 3 },
-  { title: 'Vault', url: '/assets/images/carousel/vault.png', id: 4 },
-  { title: 'Ziya', url: '/assets/images/carousel/ziya.jpeg', id: 5 },
+  // { title: 'Slow', url: '/assets/images/carousel/slow.jpeg', id: 3 },
+  // { title: 'Vault', url: '/assets/images/carousel/vault.png', id: 4 },
+  // { title: 'Ziya', url: '/assets/images/carousel/ziya.jpeg', id: 5 },
 ];
